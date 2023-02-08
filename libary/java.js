1b799a97112d7856a831844a52962cc95b3a51b2
jayden-1105 <101603388+jayden-1105@users.noreply.github.com>
2023-02-08 18:07:27 -0500

    Add files via upload

---
+++ libary/java.js
@@ -0,0 +1,23 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+        return `${title} by ${author}, ${pages} pages, ${read}`;
+    }
+}
+
+function addBookToLibrary(title, author, pages, read){
+    let newBook = new Book(title, author, pages, read)
+    myLibrary.push(newBook);
+}
+
+function getUserInfo(){
+    let title = document.getElementById("titleTextBox").value;
+    let author = document.getElementById("authorTextBox").value;
+    let pages = document.getElementById("pagesTextBox").value;
+    let read = document.getElementById("readTextBox").value;
+}
