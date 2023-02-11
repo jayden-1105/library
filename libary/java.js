26572d23d8bed8143e018b0a4df9e73940c25a05
Jayden Jones <jones.jaydenm@gmail.com>
2023-02-11 15:08:04 -0500

    Modified Form into a pop up when you click Add book that closes on submit. Also Added function that clears the library books display which fixes it duplicating books when the loop is ran again

--- libary/java.js
+++ libary/java.js
@@ -1,5 +1,25 @@
 let myLibrary = [];
+const form = document.querySelector("#newBook");
+const button = document.querySelector("#consoleLog");
+const titleInput = document.querySelector("#titleTextBox");
+const authorInput = document.querySelector("#authorTextBox");
+const pagesInput = document.querySelector("#pagesTextBox");
+const readInput = document.querySelector("#readTextBox");
+const container = document.querySelector('#container');
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    getUserInfo();
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    readInput.value = "";
+    
+});
+button.addEventListener('click', () => console.log(myLibrary));
+
+
+// Creates new Book Object
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -10,11 +30,16 @@ function Book(title, author, pages, read) {
     }
 }
 
+//Creates new Book Element and adds it to the Library
 function addBookToLibrary(title, author, pages, read){
-    let newBook = new Book(title, author, pages, read)
+    let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
+    clearLibraryDisplay();
+    addBookTile(myLibrary);
+
 }
 
+//Gets the Values entered in the Form and then plugs it into addBookToLibrary
 function getUserInfo(){
     let title = document.getElementById("titleTextBox").value;
     let author = document.getElementById("authorTextBox").value;
@@ -22,11 +47,37 @@ function getUserInfo(){
     let read = document.getElementById("readTextBox").value;
     addBookToLibrary(title, author, pages, read);
     console.log(myLibrary);
+    closeForm();
 }
 
-const form = document.querySelector("#newBook");
-form.addEventListener('submit', (e) => {
-    getUserInfo();
-});
+//Loops through the Library Array and adds it to the List on Screen
+function addBookTile(array){
+    let length = myLibrary.length;
+    for (i = 0; i < length; i++) {
+        let newBook = document.createElement("p");
+        newBook.textContent = 
+        `Title: ${myLibrary[i].title}
+        Author: ${myLibrary[i].author}
+        Pages: ${myLibrary[i].pages}
+        Read: ${myLibrary[i].read}`
+        container.appendChild(newBook);
+    }
+}
 
+//Opens Display of Form to Add New Book
+function openForm(){
+    document.getElementById("newBookForm").style.display = "block";
+}
 
+//Closes Form when you Submit the New Book
+function closeForm(){
+    document.getElementById("newBookForm").style.display = "none";
+}
+
+function clearLibraryDisplay(){
+    let child = container.lastElementChild;
+    while (child){
+        container.removeChild(child);
+        child = container.lastElementChild;
+    }
+}
